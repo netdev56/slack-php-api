5908694e164d80cf7ec528f4e4a0633ff5e9b9d3
Manuel <netdev56@gmail.com>
2021-01-28 14:59:42 +0100

    Ajout des fichiers fini

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo('Hello World !');
+require ('vendor/autoload.php');
+
+
+
+
+// Si mon url != "slack-php-api-token.herokuapp.com"
+if($_SERVER['HTTP_HOST'] != "slack-php-api-token.herokuapp.com"){
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+}
+
+// Sinon je ne le fais pas 
+
+// $ary = array('data' => "hello World"); // clé => valeur
+$ary = array('token' => $_ENV['TOKEN']);
+$data = json_encode($ary);
+
+echo($data);
